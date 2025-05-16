627f02239546176afd3f26c8934477eb80bf3336
Manu.L <manu@profil-web.fr>
2025-05-16 16:42:57 +0200

    strict mode rules stylelint

--- src/.stylelintrc.js
+++ src/.stylelintrc.js
@@ -1,16 +1,3 @@
 module.exports = {
-    "extends": "stylelint-config-standard-less",
-
-    rules: {
-        "media-query-no-invalid": null,
-        "no-descending-specificity": null,
-        "value-no-vendor-prefix" : true,
-        "selector-class-pattern": null,
-        "import-notation": "string",
-        "color-function-notation": "legacy",
-        "alpha-value-notation": null,
-        "selector-id-pattern": null,
-        "keyframes-name-pattern": null,
-        "media-feature-range-notation":"prefix",
-    }
+    "extends": "stylelint-config-standard-less"
 };
